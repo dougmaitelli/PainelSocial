d5c044dc9ff5087352c93ac88ff7559bf3150cbb
Murilo Mothsin <mothsin@hotmail.com>
2015-04-29 11:24:35 -0300

    login resources

---
+++ routes/index.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var jwt = require("jsonwebtoken");
+var config = require('../config')
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var User = require('../models/User.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.json({
+    type: true,
+    data: 'Hello Sir!'
+  });
+});
+
+router.post('/authenticate', function(req, res, next) {
+  User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
+    if (err) {
+      res.json({
+        type: false,
+        data: "Error occured: " + err
+      });
+    } else {
+      if (user) {
+        res.json({
+          type: true,
+          data: user,
+          token: user.token
+        });
+      } else {
+        res.json({
+          type: false,
+          data: "Incorrect email/password"
+        });
+      }
+    }
+  });
+});
+
+router.post('/register', function(req, res, next) {
+  if (typeof(req.body.name) == "undefined" || typeof(req.body.email) == "undefined" || typeof(req.body.password) == "undefined"){
+    res.json({ type: false, data: "Invalid data." });
+  }
+  User.findOne({email: req.body.email}, function(err, user) {
+    if (err) {
+      res.json({ type: false, data: "Error occured: " + err });
+      return ;
+    }
+    if (user) {
+      res.json({
+        type: false,
+        data: "User already exists!",
+        user: user
+      });
+      return ;
+    }
+    var userModel = new User();
+    userModel.email = req.body.email;
+    userModel.name = req.body.name;
+    userModel.password = req.body.password;
+    userModel.save(function(err, user) {
+      user.token = jwt.sign(user, config.secret, {expiresInMinutes: 1440});
+      user.save(function(err, user1) {
+        res.json({ type: true, data: user1, token: user1.token });
+      });
+    });
+  });
+});
+
+module.exports = router;
