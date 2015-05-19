d1da0cad0c34cebda976e352a17aea06e74cbfb0
Murilo Mothsin <mothsin@hotmail.com>
2015-05-19 20:31:54 -0300

    debug

--- routes/demand.js
+++ routes/demand.js
@@ -15,11 +15,11 @@ router.use(function(req, res, next) {
   var token = req.body.token || req.query.token || req.headers['x-access-token'];
   if (token) {
     jwt.verify(token, app.get('SECRET'), function(err, decoded) {
-      if (err) { return res.json({ success: false, message: 'Failed to authenticate token.' }); }
+      if (err) { return res.json({ success: false, message: 'Failed to authenticate token.', err: err }); }
       else { req.decoded = decoded; next(); }
     });
   } else {
-    return res.status(403).send({ success: false, message: token });
+    return res.status(403).send({ success: false, message: 'No token provided.' });
   }
 });
 
