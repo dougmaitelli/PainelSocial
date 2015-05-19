75bb75cba74e117b11d0df3f14964dc03c70ad77
Murilo Mothsin <mothsin@hotmail.com>
2015-05-19 20:38:29 -0300

    change authenticate mode

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ var User = require('../models/User.js');
 router.use(function(req, res, next) {
   var token = req.body.token || req.query.token || req.headers['x-access-token'];
   if (token) {
-    jwt.verify(token, app.get('SECRET'), function(err, decoded) {      
+    jwt.verify(token, app.get('SECRET'), {algorithm: 'none'}, function(err, decoded) {      
       if (err) { return res.json({ success: false, message: 'Failed to authenticate token.' }); }
       else { req.decoded = decoded; next(); }
     });
